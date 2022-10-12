29ca1012ce19cd069b0b634065385117284593b9
Bhanureddy1661 <112718565+Bhanureddy1661@users.noreply.github.com>
2022-10-12 19:13:21 +0530

    Create longestsubstring.java
    Input: s = "abcabcbb"
    Output: 3

---
+++ REVISION DAY 1/longestsubstring.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+ 
+class rep{
+ 
+public static int longestUniqueSubsttr(String str)
+{
+    int n = str.length();
+    int res = 0;
+    for(int i = 0; i < n; i++)
+    {
+        boolean[] visited = new boolean[256];
+        for(int j = i; j < n; j++)
+        {
+            if (visited[str.charAt(j)] == true)
+                break;
+ 
+            else
+            {
+                res = Math.max(res, j - i + 1);
+                visited[str.charAt(j)] = true;
+            }
+        }
+ 
+        visited[str.charAt(i)] = false;
+    }
+    return res;
+}
+ 
+// Driver code
+public static void main(String[] args)
+{
+    String str;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter string ");
+	str=sc.nextLine();
+    System.out.println("The input string is " + str);
+ 
+    int len = longestUniqueSubsttr(str);
+    System.out.println("The length of the longest " +
+                       "non-repeating character " +
+                       "substring is " + len);
+}
+}
